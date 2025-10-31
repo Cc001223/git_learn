2387f75873e749bdb5f3e26f75a702ff435ac210
caooozheng <cz001223@163.com>
2025-10-31 13:21:08 +0800

    新增文件

---
+++ Test01.java
@@ -0,0 +1,11 @@
+/**
+ * author: caooozheng 
+ * date:   2025-10-31 13:20
+ * Description:
+*/
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
